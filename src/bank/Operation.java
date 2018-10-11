0fc68f32434a6e9952777a5324f610ae9c9d5241
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-10-11 19:45:01 +0200

    Added structure for Operation and Operation Entry

--- src/bank/Operation.java
+++ src/bank/Operation.java
@@ -7,8 +7,9 @@ public abstract class Operation {
 
 
     private String name;
-    private
+    private String definition; //TODO how the definition should be stored? Maybe overriding the execute() will be sufficient;
 
 
+    //TODO should Operation operates on the account itself or should it return value (positive or negative) to add to the account balance
     public abstract OperationEntry execute();
 }
--- src/bank/OperationEntry.java
+++ src/bank/OperationEntry.java
@@ -4,4 +4,17 @@ package bank;
  * Created by Maciej on 11.10.2018.
  */
 public class OperationEntry {
+
+    private String type;
+    private String relatedProductId;
+    private String executionDate;
+    private String description;
+
+
+    public OperationEntry(String type, String relatedProductId, String executionDate, String description) {
+        this.type = type;
+        this.relatedProductId = relatedProductId;
+        this.executionDate = executionDate;
+        this.description = description;
+    }
 }
